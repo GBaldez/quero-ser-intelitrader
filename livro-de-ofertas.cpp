c74fc0d672ca52f65c86dc61df58f6059ea1e6f8
Gabriel Baldez <gabrielbaldez7@gmail.com>
2024-02-15 08:27:11 -0300

    Update livro-de-ofertas.cpp

--- livro-de-ofertas.cpp
+++ livro-de-ofertas.cpp
@@ -1,202 +1,89 @@
 #include <iostream>
+
 using namespace std;
 
-class Book
-{
+class Book {
   public:
 	double value;
 	int quantity;
 
-	void setValue(double v)
-	{
+	void setValue(double v) {
 		value = v;
 	}
-	void setQuantity(int q)
-	{
+	void setQuantity(int q) {
 		quantity = q;
-	}
-
-	Book(double v, int q)
-	{
-		value = v;
-		quantity = q;
-	}
-
-	friend std::ostream &operator<<(std::ostream &os, const Book &data)
-	{
-		os << data.value << ",";
-		os << data.quantity;
-		return os;
 	}
 };
 
-class Node
-{
-  public:
-	int position;
-	Book data;
-	Node *next;
-
-	Node(const Book &properties, int p) : data(properties), next(nullptr)
-	{
-		position = p;
-	}
-};
-
-void printList(Node *n)
-{
-	while (n != nullptr)
-	{
-		cout << n->position << "," << n->data << endl;
-		n = n->next;
-	}
-}
-
-void insertAtHead(Node **head, int position, Book newBook)
-{
-	Node *newNode = new Node(newBook, position);
-	newNode->position = 1;
-	newNode->data = newBook;
-	newNode->next = *head;
-	*head = newNode;
-}
-
-void insertAt(Node **head, int position, Book newBook)
-{
-	Node *newNode = new Node(newBook, position);
-	newNode->data = newBook;
-
-	Node *last = *head;
-	Node *previous;
-
-	while (last->next != nullptr && last->position < position)
-	{
-		previous = last;
-		last = last->next;
-	}
-	if (last->position == position)
-	{
-		newNode->position = last->position;
-		newNode->next = last->next;
-		previous->next = newNode;
-	}
-	else
-	{
-		newNode->position = last->position + 1;
-		newNode->next = last->next;
-		last->next = newNode;
-	}
-}
-
-Node *search(Node *head, int position)
-{
-	Node *temp = head;
-	while (temp != nullptr)
-	{
-		if (temp->position == position)
-		{
-			return temp;
+void printBooks(Book* offerBook, int size) {
+		for (int i = 0; i < size; i++) {
+		  cout << i + 1 << ",";
+	  	cout << offerBook[i].value<< ",";
+		
+		if(i == size -1) {
+		  cout << offerBook[i].quantity<< "\\";
+		} else {
+		  cout << offerBook[i].quantity<< "\n";
 		}
-		temp = temp->next;
-	}
-	return nullptr;
-}
-
-void deleteNode(Node *&head, Node *previous, Node *toRemove)
-{
-	if (toRemove == head)
-	{
-		head = toRemove->next;
-		delete toRemove;
-	}
-	else if (toRemove)
-	{
-		previous->next = toRemove->next;
-		delete toRemove;
-	}
-	Node *temp = head;
-	int newPos = 1;
-	while (temp != nullptr)
-	{
-		temp->position = newPos;
-		temp = temp->next;
-		newPos++;
 	}
 }
 
-int main(int argc, char *argv[])
-{
-	Node *head = nullptr;
-	Node *temp = nullptr;
-
-	int notifications = 0;
-
-	cout << "Insert the input ";
-	cin >> notifications;
-
-	while (notifications > 0)
-	{
-		int position;
-		int action;
-		double value;
-		int quantity;
-
+void deleteAt(int position, Book* offerBook, int size) {
+        for (int i = position; i < size; i++) {
+          offerBook[i - 1] = offerBook[i];
+        }
+        size--;
+}
+    
+int main(int argc, char *argv[]) {
+	int updates;
+	int size;
+	int position;
+	int action;
+	
+	cout << "Insert the input:  "<< endl;
+	cin >> updates;
+	size = updates;
+	
+	Book* offerBook = new Book[updates];
+	
+	for (int i = 0; i < updates; i++) {
+		Book properties;
+		
 		cin >> position;
 		cin.ignore(1, ',');
 		cin >> action;
 		cin.ignore(1, ',');
-		cin >> value;
+		cin >> properties.value;
 		cin.ignore(1, ',');
-		cin >> quantity;
-
-		switch (action)
-		{
-		case 0:
-			if (head == nullptr || position == 1)
-			{
-				Book newBook(value, quantity);
-				insertAtHead(&head, position, newBook);
+		cin >> properties.quantity;
+		
+		switch (action) {
+		  case 0:
+			if(offerBook[position -1].value == 0) {
+			  size--;
 			}
-			else
-			{
-				Book newBook(value, quantity);
-				insertAt(&head, position, newBook);
-			}
+		    offerBook[position -1] = properties;	
 			break;
 		case 1:
-
-			temp = search(head, position);
-
-			if (!temp)
-			{
-				cout << "Don't have any element in this position!" << endl;
-			}
-			else
-			{
-				if (value > 0)
-				{
-					temp->data.setValue(value);
-				}
-
-				if (quantity > 0)
-				{
-					temp->data.setQuantity(quantity);
-				}
-			}
-			break;
+		  if (properties.value > 0) {
+    		offerBook[position - 1].setValue(properties.value);
+		  }
+          if (properties.quantity > 0) {
+    	    offerBook[position - 1].setQuantity(properties.quantity);
+		  }
+		  size--;
+	  	break;
 		case 2:
-			if (head)
-			{
-				temp = search(head, position);
-				Node *previous = search(head, position - 1);
-
-				deleteNode(head, previous, temp);
-			}
+		deleteAt(position, offerBook, size);
 			break;
 		}
-
-		notifications--;
-	}
-
-	printList(head);
+ 	}
+ 	
+ 	printBooks(offerBook, size);
+ 	
+ 	delete[] offerBook;
+ 	offerBook = nullptr;
+ 	
+ 	cin.get();
 }
