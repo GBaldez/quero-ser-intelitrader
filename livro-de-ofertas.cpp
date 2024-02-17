718dae76b81554912db8884bfc959b0d1dc1c59a
Gabriel Baldez <gabrielbaldez7@gmail.com>
2024-02-17 16:40:47 -0300

    Update livro-de-ofertas.cpp

--- livro-de-ofertas.cpp
+++ livro-de-ofertas.cpp
@@ -3,87 +3,125 @@
 using namespace std;
 
 class Book {
-  public:
-	double value;
-	int quantity;
+public:
+  int position;
+  double value;
+  int quantity;
 
-	void setValue(double v) {
-		value = v;
-	}
-	void setQuantity(int q) {
-		quantity = q;
-	}
+  void setValue(double v) { value = v; }
+  void setQuantity(int q) { quantity = q; }
 };
 
-void printBooks(Book* offerBook, int size) {
-		for (int i = 0; i < size; i++) {
-		  cout << i + 1 << ",";
-	  	cout << offerBook[i].value<< ",";
-		
-		if(i == size -1) {
-		  cout << offerBook[i].quantity<< "\\";
-		} else {
-		  cout << offerBook[i].quantity<< "\n";
-		}
-	}
+class List {
+public:
+  int size;
+  Book *offerBook;
+};
+
+void inicializeList(List *list) { list->size = 0; }
+
+void printList(List *list) {
+  for (int i = 0; i < list->size; i++) {
+    if (i == list->size - 1) {
+      cout << list->offerBook[i].position << "," << list->offerBook[i].value
+           << "," << list->offerBook[i].quantity << "\\" << endl;
+    } else {
+      cout << list->offerBook[i].position << "," << list->offerBook[i].value
+           << "," << list->offerBook[i].quantity << endl;
+    }
+  }
 }
 
-void deleteAt(int position, Book* offerBook, int size) {
-        for (int i = position; i < size; i++) {
-          offerBook[i - 1] = offerBook[i];
-        }
-        size--;
+bool insertBook(List *list, Book book, int position) {
+  if (position < 0) {
+    return false;
+  }
+  list->size++;
+  int index = position - 1;
+  for (int i = list->size; i > index; i--) {
+    list->offerBook[i] = list->offerBook[i - 1];
+    list->offerBook[i].position++;
+  }
+  list->offerBook[index] = book;
+  return true;
 }
-    
+
+int searchBook(List *list, int position) {
+  int i = 0;
+  while (i < list->size) {
+    if (list->offerBook[i].position == position) {
+      return i;
+    }
+    i++;
+  }
+  return -1;
+}
+
+bool uptadeBook(List *list, int position, int quantity, double value) {
+  int index = searchBook(list, position);
+  if (index == -1) {
+    return false;
+  }
+  if (quantity > 0) {
+    list->offerBook[index].setQuantity(quantity);
+  }
+  if (value > 0) {
+    list->offerBook[index].setValue(value);
+  }
+  return true;
+}
+
+bool removeBook(List *list, int position) {
+  int index = searchBook(list, position);
+  if (index == -1) {
+    return false;
+  }
+  for (int i = index; i < list->size - 1; i++) {
+    list->offerBook[i] = list->offerBook[i + 1];
+    list->offerBook[i].position--;
+  }
+  list->size--;
+  return true;
+}
+
 int main(int argc, char *argv[]) {
-	int updates;
-	int size;
-	int position;
-	int action;
-	
-	cout << "Insert the input:  "<< endl;
-	cin >> updates;
-	size = updates;
-	
-	Book* offerBook = new Book[updates];
-	
-	for (int i = 0; i < updates; i++) {
-		Book properties;
-		
-		cin >> position;
-		cin.ignore(1, ',');
-		cin >> action;
-		cin.ignore(1, ',');
-		cin >> properties.value;
-		cin.ignore(1, ',');
-		cin >> properties.quantity;
-		
-		switch (action) {
-		  case 0:
-			if(offerBook[position -1].value == 0) {
-			  size--;
-			}
-		    offerBook[position -1] = properties;	
-			break;
-		case 1:
-		  if (properties.value > 0) {
-    		offerBook[position - 1].setValue(properties.value);
-		  }
-          if (properties.quantity > 0) {
-    	    offerBook[position - 1].setQuantity(properties.quantity);
-		  }
-		  size--;
-	  	break;
-		case 2:
-		deleteAt(position, offerBook, size);
-			break;
-		}
- 	}
- 	
- 	printBooks(offerBook, size);
- 	
- 	delete[] offerBook;
- 	offerBook = nullptr;
- 	
- 	cin.get();
+  int updates;
+  int action;
+
+  cout << "Insert the input:  " << endl;
+  cin >> updates;
+
+  Book *books = new Book[updates];
+  List offerBook;
+  offerBook.offerBook = books;
+  inicializeList(&offerBook);
+
+  for (int i = 0; i < updates; i++) {
+    Book properties;
+
+    cin >> properties.position;
+    cin.ignore(1, ',');
+    cin >> action;
+    cin.ignore(1, ',');
+    cin >> properties.value;
+    cin.ignore(1, ',');
+    cin >> properties.quantity;
+
+    switch (action) {
+    case 0:
+      insertBook(&offerBook, properties, properties.position);
+      break;
+    case 1:
+      uptadeBook(&offerBook, properties.position, properties.quantity,
+                 properties.value);
+      break;
+    case 2:
+      removeBook(&offerBook, properties.position);
+      break;
+    }
+  }
+
+  printList(&offerBook);
+
+  cin.get();
 }
